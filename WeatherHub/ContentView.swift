0fa22ea271bd55acda9bae419fbf9515967d1fb9
Armando Brito <arebrif@gmail.com>
2022-12-06 18:42:24 -0400

    Initial Commit

---
+++ WeatherHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherHub
+//
+//  Created by Armando Brito on 6/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherHub/WeatherHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherHubApp.swift
+//  WeatherHub
+//
+//  Created by Armando Brito on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
